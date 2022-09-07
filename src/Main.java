35e911f317ec6e9f66f8669c2616d1965635a930
user <etaryan2001@mail.ru>
2022-09-07 22:32:11 +0400

    frst project

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+       //varName = value;
+        byte b1 = 127;
+         short s =500;
+         int i =448154685;
+         float f;
+         f= 12.5F;
+         double d;
+         d = 148.5;
+         char ch1 = 'Y';
+         char ch2 = 'L';
+         boolean b;
+         b = false;
+         boolean t = true;
+         System.out.println("b1 is" + b1);
+         System.out.println("s is " + s);
+         System.out.println("i is " + i);
+         System.out.println("f is " + f);
+         System.out.println("d is" + d );
+         System.out.println("ch1 is" + ch1);
+         System.out.println("ch2 is" + ch2);
+         System.out.println("boolean is " + b);
+    }
+    }
+
+
