dc1aa8ec5a4d1ef53286bb2534b4f54e741bd500
user <etaryan2001@mail.ru>
2022-09-11 13:41:21 +0400

    frst project

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,41 @@ public class Main {
     public static void main(String[] args) {
        //varName = value;
         byte b1 = 127;
-         short s =500;
-         int i =448154685;
+        byte b2 = 111;
+         short s = 500;
+         short s1= 25999;
+         int i = 448154685;
+         int i1 = -6598456;
+         long c = 5486491864294654L;
+         long c1 = -4826848984828L;
          float f;
          f= 12.5F;
+         float f1 = -464545248545F;
          double d;
          d = 148.5;
          char ch1 = 'Y';
          char ch2 = 'L';
+         char ch3 = 8;
          boolean b;
          b = false;
-         boolean t = true;
-         System.out.println("b1 is" + b1);
+         boolean t;
+           t = true;
+         System.out.println("b1 is " + b1);
          System.out.println("s is " + s);
          System.out.println("i is " + i);
          System.out.println("f is " + f);
-         System.out.println("d is" + d );
-         System.out.println("ch1 is" + ch1);
-         System.out.println("ch2 is" + ch2);
+         System.out.println("f1 is " + f1);
+         System.out.println("d is " + d );
+         System.out.println("ch1 is " + ch1);
+         System.out.println("ch2 is " + ch2);
+         System.out.println("ch3 is " + ch3);
          System.out.println("boolean is " + b);
+         System.out.println("boolean is " + t);
+         System.out.println("b2 is " + b2);
+         System.out.println("s1 is " + s1);
+         System.out.println("i1 is " + i1);
+         System.out.println("c is " + c);
+         System.out.println("c1 is " + c1);
     }
     }
 
