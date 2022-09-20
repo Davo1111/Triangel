44fa1ed241510bbd05525bb1c228da3464fca71b
user <etaryan2001@mail.ru>
2022-09-21 00:48:57 +0400

    homework 11

--- src/test.java
+++ src/test.java
@@ -1,2 +1,48 @@
+import java.sql.SQLOutput;
+
 public class test {
+    public static void main(String[] args) {
+        int day = 6;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+            default:
+                System.out.println("It is good");
+
+                //Max value
+                int a = 12, b = 24, c = 50, d = 66;
+                int max = b;
+
+                if (a > max) ;
+                max = a;
+                if (c > max) ;
+                max = c;
+                if (d > max) ;
+                max = d;
+                System.out.println("max is " + max);
+                //Triangel
+        }
+        int side1 = 12;
+        int side2 = 24;
+        int side3 = 33;
+
+        if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) ;
+        System.out.println("Triangle");
+    }
 }
